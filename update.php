a4324491e151f4b2972df1e58c2bf4bafa364708
MbouRowin <117640285+MbouRowin@users.noreply.github.com>
2023-02-24 12:04:09 +0100

    Update

--- update.php
+++ update.php
@@ -10,32 +10,28 @@ if (!$id) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $kleur1 = $_POST["kleur-1"] ?? "";
-    $kleur2 = $_POST["kleur-2"] ?? "";
-    $kleur3 = $_POST["kleur-3"] ?? "";
-    $kleur4 = $_POST["kleur-4"] ?? "";
-    $basiskleuren = implode(", ", [$kleur1, $kleur2, $kleur3, $kleur4]);
-    $tel = $_POST["tel"] ?? "";
+    $homeclub = $_POST["homeclub"] ?? "";
+    $lidmaatschap = $_POST["lidmaatschap"] ?? "";
+    $looptijd = $_POST["looptijd"] ?? "";
+    $extra = $_POST["extra"] ?? [];
+    $extra = implode(", ", $extra);
     $email = $_POST["email"] ?? "";
-    $afspraakdatum = $_POST["datum"] ?? "";
-    $behandeling = $_POST["behandeling"] ?? [];
-    $behandeling = implode(", ", $behandeling);
-    $now = $_POST["now"] ?? "";
 
-    $stmt = $pdo->prepare("UPDATE afspraak SET basiskleuren = ?, tel = ?, email = ?, afspraakdatum = ?, behandeling = ?, datum = ? WHERE Id = ?");
-    $stmt->bindValue(1, $basiskleuren);
-    $stmt->bindValue(2, $tel);
-    $stmt->bindValue(3, $email);
-    $stmt->bindValue(4, $afspraakdatum);
-    $stmt->bindValue(5, $behandeling);
-    $stmt->bindValue(6, $now);
-    $stmt->bindValue(7, $id);
+    $stmt = $pdo->prepare("UPDATE inschrijving SET homeclub = ?, lidmaatschap = ?, looptijd = ?, extra = ?, email = ? WHERE id = ?");
+    $stmt->bindValue(1, $homeclub);
+    $stmt->bindValue(2, $lidmaatschap);
+    $stmt->bindValue(3, $looptijd);
+    $stmt->bindValue(4, $extra);
+    $stmt->bindValue(5, $email);
+    $stmt->bindValue(6, $id);
 
     $stmt->execute();
 
     header("Refresh: 2; url=/read.php");
-    die("De afspraak is aangepast!");
+    die("De inschrijving is aangepast!");
 }
+
+$vestigingen = $pdo->query("SELECT * FROM vestiging");
 
 ?>
 <!doctype html>
@@ -54,54 +50,70 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <h1>Update</h1>
             <a href="read.php">Read</a>
 
-            <form method="post" class="mt-1">
-                <div>
-                    Kies 4 basiskleuren voor uw nagels:
-                    <div class="row mt-1">
-                        <input type="color" name="kleur-1" value="#ff0000">
-                        <input type="color" name="kleur-2" value="#ffffff">
-                        <input type="color" name="kleur-3" value="#0000ff">
-                        <input type="color" name="kleur-4" value="#ffa500">
+            <form method="post">
+                <div class="mt-2">
+                    Kies je homeclub:
+
+                    <select class="mt-1 select" name="homeclub" id="homeclub">
+                        <?php while ($row = $vestigingen->fetch()) : ?>
+                            <?php $value = $row["straatnaam"] . " " . $row["huisnummer"] ?>
+                            <option value="<?= $value ?>"><?= $value ?></option>
+                        <?php endwhile ?>
+                    </select>
+                </div>
+
+                <div class="mt-2">
+                    Selecteer een lidmaatschap:
+
+                    <div class="mt-1">
+                        <input type="radio" name="lidmaatschap" id="comfort" value="Comfort">
+                        <label for="comfort">Comfort</label>
+
+                        <input type="radio" name="lidmaatschap" id="premium" value="Premium">
+                        <label for="premium">Premium</label>
+
+                        <input type="radio" name="lidmaatschap" id="all-in" value="All in">
+                        <label for="all-in">All in</label>
                     </div>
                 </div>
 
-                <div class="mt-1">
-                    <label for="tel" class="block">Uw telefoonnummer:</label>
-                    <input type="tel" name="tel" id="tel" class="input" pattern="[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}" required>
+                <div class="mt-2">
+                    Selecteer een looptijd:
+
+                    <div class="mt-1">
+                        <input type="radio" name="looptijd" id="jaar" value="Jaarlidmaatschap">
+                        <label for="jaar">Jaarlidmaatschap</label>
+
+                        <input type="radio" name="looptijd" id="flex" value="Flex optie">
+                        <label for="flex">Flex optie</label>
+                    </div>
                 </div>
 
-                <div class="mt-1">
+                <div class="mt-2">
+                    Selecteer je extra's:
+
+                    <div class="mt-1">
+                        <input type="checkbox" name="extra[]" value="Yanga sportswater" id="sportswater">
+                        <label for="sportswater" class="">Yanga sportswater $2,50 per 4 weken</label>
+                    </div>
+
+                    <div class="mt-1">
+                        <input type="checkbox" name="extra[]" value="Personal online coach" id="online-coach">
+                        <label for="online-coach" class="">Personal online coach $60 eenmalig</label>
+                    </div>
+
+                    <div class="mt-1">
+                        <input type="checkbox" name="extra[]" value="Personal training intro" id="training-intro">
+                        <label for="training-intro" class="">Personal training intro $25 eenmalig</label>
+                    </div>
+                </div>
+
+                <div class="mt-2">
                     <label for="email" class="block">Uw e-mailadres:</label>
-                    <input type="email" name="email" id="email" class="input" required>
+                    <input type="email" name="email" id="email" class="input mt-1" required>
                 </div>
 
-                <div class="mt-1">
-                    <label for="land" class="block">Afspraak datum:</label>
-                    <input type="datetime-local" name="datum" id="datum" class="input" required>
-                </div>
-
-                <div class="mt-1">
-                    Soort behandeling:
-
-                    <div class="mt-1">
-                        <input type="checkbox" name="behandeling[]" value="Nagelbijt" id="nagelbijt">
-                        <label for="nagelbijt" class="">Nagelbijt arrangement (termijnbetaling mogelijk) $180</label>
-                    </div>
-
-                    <div class="mt-1">
-                        <input type="checkbox" name="behandeling[]" value="Luxe manicure" id="manicure">
-                        <label for="manicure" class="">Luxe manicure (massage en handpakking) $30</label>
-                    </div>
-
-                    <div class="mt-1">
-                        <input type="checkbox" name="behandeling[]" value="Nagelreparatie" id="reparatie">
-                        <label for="reparatie" class="">Nagelreparatie per nagel (in eerste week gratis) $5</label>
-                    </div>
-                </div>
-
-                <input type="hidden" name="now" value="<?= date(DATE_RFC2822); ?>">
-
-                <div class="row mt-1">
+                <div class="row mt-2">
                     <button class="button">Sla op</button>
                     <button type="reset" class="button">Reset</button>
                 </div>
